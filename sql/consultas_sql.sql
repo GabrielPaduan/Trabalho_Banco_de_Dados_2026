fdd23e00c7b5a64f4318627fd42d444c70c922de
Paduan <gabrielpaduancofani@gmail.com>
2026-05-11 23:36:54 -0300

    feat: adiciona scripts de criação, população e manipulação do banco de dados

---
+++ sql/consultas_sql.sql
@@ -0,0 +1,10 @@
+SELECT * FROM feature_store.feature WHERE id_versao = 1;
+
+SELECT v_filha.numero_versao AS versao_atual,
+	   v_filha.arquivo_path, v_base.numero_versao AS derivada_da_versao
+FROM feature_store.versao v_filha
+LEFT JOIN feature_store.versao v_base ON v_filha.id_versao_base = v_base.id;
+
+SELECT d.nome AS dataset, d.descricao, u.nome AS criador
+FROM feature_store.dataset d
+JOIN feature_store.usuario u ON d.cpf_usuario = u.cpf
