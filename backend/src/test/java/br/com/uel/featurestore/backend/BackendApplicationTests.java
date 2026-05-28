9faaf49b031fce18cd9a1a62233c21997aa36b6d
Paduan <gabrielpaduancofani@gmail.com>
2026-05-28 11:53:03 -0300

    docs: criação do ambiente spring visando arquitetura MVC

---
+++ backend/src/test/java/br/com/uel/featurestore/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uel.featurestore.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
